c4eaca967cdaaf95958b874a4248de7fc5aec608
HamletGalstyan <51154878+HamletGalstyan@users.noreply.github.com>
2019-06-11 11:05:08 +0400

    Add files via upload

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 10, 5, 2, 3, 4 };
+
+            ShowNumbers(numbers);
+            ShowNumbers(SelectionSort(numbers));
+            Console.ReadKey();
+        }
+
+        public static int[] SelectionSort(int[] numbers)
+        {
+            int indexMin, temp;
+
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                indexMin = i;
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[indexMin])
+                    {
+                        indexMin = j;
+                    }
+                }
+
+                if(indexMin!=i)
+                {
+                    temp = numbers[i];
+                    numbers[i] = numbers[indexMin];
+                    numbers[indexMin] = temp;
+                }
+
+
+            }
+
+            return numbers;
+        }
+
+
+
+        public static void ShowNumbers(int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
